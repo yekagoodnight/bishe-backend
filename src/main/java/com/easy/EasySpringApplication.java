1a61a9d72dae22cb0f61ce75e143cb7bdb5b2823
yekagoodnight <750666491@qq.com>
2025-04-27 17:37:43 +0800

    1

--- src/main/java/com/easy/EasySpringApplication.java
+++ src/main/java/com/easy/EasySpringApplication.java
@@ -3,6 +3,7 @@ package com.easy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// hello
 @SpringBootApplication
 public class EasySpringApplication {
 	public static void main(String[] args) throws Exception {
